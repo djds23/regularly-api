57022d01f7c2cf074573e079f868cec0219cdde2
Dean Silfen <dean.silfen@gmail.com>
2016-07-25 23:23:49 -0400

    add from/to params to calendar controller

--- app/controllers/api/v1/calendar_controller.rb
+++ app/controllers/api/v1/calendar_controller.rb
@@ -2,7 +2,15 @@ module Api
   module V1
     class CalendarController < ApplicationController
       def index
-        render json: CalendarPresenter.new
+        presenter = CalendarPresenter.new(
+          from: params.fetch(:from, Time.now.next_week.monday),
+          to: params.fetch(:to, 3.months.ago)
+        )
+        render json: presenter
+      end
+
+      def album_params
+        params.permit(:to, :from)
       end
     end
   end
