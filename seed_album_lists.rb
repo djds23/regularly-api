4900f4c1d57e22ac79d0e80828d718768cfa6063
Dean Silfen <dean.silfen@gmail.com>
2016-08-15 13:36:38 -0400

    Update seed album list

--- seed_album_lists.rb
+++ seed_album_lists.rb
@@ -98,3 +98,15 @@ if add.album.blank?
   add.save!
 end
 
+add = add.next
+if add.album.blank?
+  builder = AlbumArtistBuilder.new(album_name: 'Tragic Kingdom', artist_name: 'No Doubt')
+  builder.add_embed(
+    service_name: 'spotify',
+    embed: '<iframe src="https://embed.spotify.com/?uri=spotify%3Aalbum%3A3VekjWskUut57hx6W9wqL8" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+  )
+  artist = builder.build
+  add.album = artist.albums.first
+  add.save!
+end
+
