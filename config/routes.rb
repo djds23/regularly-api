f885c244788c7a1d156aec0c9be68b48431538b0
Dean Silfen <dean.silfen@gmail.com>
2017-05-14 09:00:29 -0400

    Still trying to get clearance working

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,36 @@
 Rails.application.routes.draw do
-  constraints Clearance::Constraints::SignedIn.new { |user| user.admin? } do
-    namespace :admin do
-      resources :albums
-      resources :album_due_dates
-      resources :album_embeds
-      resources :artists
-      resources :users
-      resources :user_edges
+  scope '/site' do
+    resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+    resource :session, controller: "clearance/sessions", only: [:create]
 
-      root to: "albums#index"
+    resources :users, controller: "clearance/users", only: [:create] do
+      resource :password,
+        controller: "clearance/passwords",
+        only: [:create, :edit, :update]
     end
+
+    get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+    delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+    get "/sign_up" => "clearance/users#new", as: "sign_up"
+
+    constraints Clearance::Constraints::SignedIn.new { |user| user.admin? } do
+      namespace :admin do
+        resources :albums
+        resources :album_due_dates
+        resources :album_embeds
+        resources :artists
+        resources :users
+        resources :user_edges
+
+        root to: "albums#index"
+      end
+    end
+
+    resources :user_edges
+    resources :album_embeds
+    resources :albums
+    resources :users
+    resources :artists
   end
 
   namespace :api do
@@ -18,11 +39,5 @@ Rails.application.routes.draw do
       resources :album_due_dates, path: :posts
     end
   end
-
-  resources :user_edges
-  resources :album_embeds
-  resources :albums
-  resources :users
-  resources :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
